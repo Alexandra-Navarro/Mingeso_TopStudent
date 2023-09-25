73681bc644562d1603675cb3db9954e50402e52b
Alexandra Navarro <alexandra.navarro@usach.cl>
2023-09-25 19:51:15 -0300

    Comienzo del proyecto

---
+++ src/main/java/com/example/ProyectoTE/ProyectoTeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoTE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoTeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProyectoTE/ProyectoTeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoTE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoTeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
